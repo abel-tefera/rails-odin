6ba50a5f823f29bc676d65045b348a0b484dd304
abel-tefera <abeltefera16@gmail.com>
2023-07-31 12:55:28 +0300

    link to

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
 
+  resources :articles
   resources :cars
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
