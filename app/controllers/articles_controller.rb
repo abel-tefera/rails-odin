37d9fb46ded475f1360435075dacf15c480e5f2a
abel-tefera <abeltefera16@gmail.com>
2023-07-31 18:23:45 +0300

    auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "abel", password: "12345", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
