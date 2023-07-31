968398b7b011511bc10309f1123232213702707d
abel-tefera <abeltefera16@gmail.com>
2023-07-31 16:14:46 +0300

    Status

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_31_111130) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_31_130045) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "cars", force: :cascade do |t|
@@ -32,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_31_111130) do
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
