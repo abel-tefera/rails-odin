d49faae00162e5904bda27a1cb8321f654f8347a
abel-tefera <abeltefera16@gmail.com>
2023-07-31 11:31:18 +0300

    Articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "/articles", to: "articles#index"
+
   resources :cars
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
